c3030a50286af718c86e4f6ce16eb8bd4abe25a4
linaigao <linaigao@berkeley.edu>
2025-07-27 17:58:41 -0700

    Implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  // @ts-expect-error expected
+  publicRoutes: ["/api/webhooks/clerk"],
+});
 
 export const config = {
   matcher: [
