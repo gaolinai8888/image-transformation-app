9c368501d2252188d029d4f4513e8231a5e937df
linaigao <linaigao@berkeley.edu>
2025-07-27 18:54:34 -0700

    Fix middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,32 @@
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware({
+//   // @ts-expect-error expected
+//   publicRoutes: ["/api/webhooks/clerk"],
+// });
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
+
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  // @ts-expect-error expected
-  publicRoutes: ["/api/webhooks/clerk"],
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
+    // Protect everything except these public routes:
+    /*
+      - Next.js internals (_next)
+      - Static files
+      - favicon.ico
+      - Your webhook route, so it's public
+    */
+    "/((?!_next/static|_next/image|favicon.ico|api/webhooks/clerk).*)",
   ],
 };
