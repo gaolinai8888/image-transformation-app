69156066716e0ca0995644dbb890bec0f4ffe305
linaigao <linaigao@berkeley.edu>
2025-07-29 23:38:16 -0700

    Implement image upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={`${cn("font-IBMPlex", IBMPlex.variable)} antialiased`}>
           {children}
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
